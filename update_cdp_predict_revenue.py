22ea5f777ee79dbcfbcae6f61ca1e1b57adc9ab8
pine <ychanc2104@gmail.com>
2021-11-12 12:46:18 +0800

    #18 remove not found product_id to be inserted

--- update_cdp_predict_revenue.py
+++ update_cdp_predict_revenue.py
@@ -8,6 +8,14 @@ import socket
 from basic.date import get_today, datetime_to_str
 
 
+def add_title_clean(df):
+    df_copy = df.copy()
+    titles = df['title']
+    titles_clean = [title.replace("'", '"') for title in titles]
+    df_copy['title_clean'] = titles_clean
+    return df_copy
+
+
 
 # update everyday
 if __name__ == '__main__':
@@ -29,9 +37,11 @@ if __name__ == '__main__':
         if np.array(df).size == 0:
             print(f"this web_id {web_id} without data in cdp")
             continue
+
+        df_clean = add_title_clean(df)  ## replace ' with "
         titles_all = list(set(df['title']))
         n = len(titles_all)
-        dict_map = ecom.fetch_product_id_by_title(titles_all)
+        # dict_map = ecom.fetch_product_id_by_title(titles_all)
         dict_predict = {}
         i = 0
         for title in titles_all:
@@ -41,10 +51,13 @@ if __name__ == '__main__':
             revenue_L7 = np.mean(restored_sig[-14:-7])  # last 7-day mean revenue (known)
             revenue_F7 = np.mean(restored_sig[-7:])  # future 7-day mean revenue (predict)
             diff = revenue_F7 - revenue_L7
-            if title in dict_map:
-                product_id = dict_map[title]
+            title_clean = title.replace("'", '"')
+            df_search_id = df_clean.query(f"title_clean=='{title_clean}'")
+            if np.array(df_search_id).size == 0:
+                product_id = '_' ## not found
             else:
-                product_id = '_'
+                product_id = df_search_id['product_id'].values[0]
+
             if revenue_L7 == 0:
                 grow_rate = 0
             else:
@@ -54,7 +67,8 @@ if __name__ == '__main__':
             i += 1
 
         df_to_insert = pd.DataFrame.from_dict(dict_predict, "index")
-        data_list_of_dict = df_to_insert.to_dict('records')
+        df_to_insert_clean = df_to_insert.drop(index=df_to_insert[df_to_insert['product_id'] == '_'].index, inplace=False)
+        data_list_of_dict = df_to_insert_clean.to_dict('records')
         # MySqlHelper('cdp').ExecuteInsert('cdp_predict_revenue', data_list_of_dict)
         query = f"REPLACE INTO cdp.cdp_predict_revenue " \
                 f"(web_id,product_id,title,revenue_L7,revenue_F7,grow_rate,date) " \
